049a08a6ce839cb65cb04e17acbd37f03591c69e
2vaishnavi-ks <vaishnaviksharath@gmail.com>
2024-11-18 13:34:06 +0530

    Add files via upload

---
+++ mainCounter.java
@@ -0,0 +1,24 @@
+class counter extends Thread
+{
+     static int count;
+     public void run()
+     {
+           for(int i=0;i<1000;i++)
+           {
+                count++;
+           }
+     }
+}
+class mainCounter
+{
+         public static void main(String args[])
+         {
+                   counter t1 =new counter();
+                   counter t2 =new counter();
+                   
+                   t1.start();
+                   t2.start();
+                   
+                   System.out.println("final count is:"+ counter.count);
+         }
+}
